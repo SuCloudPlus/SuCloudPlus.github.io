2d65d1f659a30a467bb398b7eb9cf54a0a546ce6
SuCloudPlus <121447640+SuCloudPlus@users.noreply.github.com>
2026-01-14 16:43:27 +0800

    index页面参数配置

---
+++ assets/links-config.js
@@ -0,0 +1,31 @@
+// index页面参数配置
+const LINKS_CONFIG = {
+    "links": [
+        {
+            "title": "加密书签管理器",
+            "description": "个人加密书签管理工具",
+            "url": "https://sucloudplus.github.io/tools/bookmark"
+        },
+        {
+            "title": "加密记事本",
+            "description": "个人加密记事本",
+            "url": "https://sucloudplus.github.io/tools/note"
+        },
+        {
+            "title": "音频可视化视频生成器",
+            "description": "制作音频可视化视频",
+            "url": "https://sucloudplus.github.io/tools/videomod"
+        },
+        {
+            "title": "文字模板生成器",
+            "description": "生成定制文字模板图片",
+            "url": "https://sucloudplus.github.io/tools/textmod"
+        },
+        {
+            "title": "文本行数显示器",
+            "description": "显示粘贴的文本的行数",
+            "url": "https://sucloudplus.github.io/tools/textlines"
+        }
+        // 在这里添加更多链接
+    ]
+};
